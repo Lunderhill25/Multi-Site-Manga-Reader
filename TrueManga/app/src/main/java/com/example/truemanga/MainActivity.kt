8178b81c24ff9b324f31725d2bcc10b1831f60e4
Lunderhill25 <72555440+Lunderhill25@users.noreply.github.com>
2022-11-01 12:56:49 +0000

    Revert "Attempted To Add Icons To Tab Structure"
    This reverts commit 3dd6339c2a6c34ede746b0be30e1923f5fd4506c.

--- TrueManga/app/src/main/java/com/example/truemanga/MainActivity.kt
+++ TrueManga/app/src/main/java/com/example/truemanga/MainActivity.kt
@@ -2,29 +2,10 @@ package com.example.truemanga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.truemanga.fragments.FavouritesFragment
-import com.example.truemanga.fragments.SettingsFragment
-import com.example.truemanga.fragments.SitesFragment
-import com.example.truemanga.fragments.adapters.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setUpTabs()
-    }
-
-    private fun setUpTabs() {
-        val adapter = ViewPagerAdapter(supportFragmentManager)
-        adapter.addFragment(SitesFragment(),"")
-        adapter.addFragment(FavouritesFragment(),"")
-        adapter.addFragment(SettingsFragment(),"")
-        viewPager.adapter
-        tabs.setupWithViewPager(viewPager)
-
-        tabs.getTabAt(0)!!setIcon(R.drawable.ic_baseline_web_24)
-        tabs.getTabAt(1)!!setIcon(R.drawable.ic_baseline_star_24)
-        tabs.getTabAt(2)!!setIcon(R.drawable.ic_baseline_settings_24)
     }
 }
