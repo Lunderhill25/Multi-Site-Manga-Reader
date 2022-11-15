629f7c3c6771e46ba50cdf0091d0ffecb09032a5
Lunderhill25 <72555440+Lunderhill25@users.noreply.github.com>
2022-11-15 16:02:33 +0000

    Small Changes Made

--- TrueManga/app/src/main/java/com/example/truemanga/MainActivity.kt
+++ TrueManga/app/src/main/java/com/example/truemanga/MainActivity.kt
@@ -2,24 +2,37 @@ package com.example.truemanga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.truemanga.fragments.FavouritesFragment
 import com.example.truemanga.fragments.SettingsFragment
 import com.example.truemanga.fragments.SitesFragment
 import com.example.truemanga.fragments.adapters.ViewPagerAdapter
+import com.google.android.material.switchmaterial.SwitchMaterial
+
+///import kotlinx.android.synthetic.main.<activity_main>.*;
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setUpTabs()
+
     }
+
+
+
 
     private fun setUpTabs(){
         val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(SitesFragment(),"")
         adapter.addFragment(FavouritesFragment(),"")
         adapter.addFragment(SettingsFragment(),"")
-        adapter.addFragment(SitesFragment(),"")
 
         val viewPager = findViewById<androidx.viewpager.widget.ViewPager>(R.id.view_pager)
         val tabLayout = findViewById<com.google.android.material.tabs.TabLayout>(R.id.tab_layout)
@@ -34,4 +47,7 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+
+
 }
--- TrueManga/app/src/main/java/com/example/truemanga/fragments/SettingsFragment.kt
+++ TrueManga/app/src/main/java/com/example/truemanga/fragments/SettingsFragment.kt
@@ -5,13 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import com.example.truemanga.R
+//import com.example.truemanga.databinding.FragmentSettingsBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 
 class SettingsFragment : Fragment() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val switch:SwitchMaterial? = view?.findViewById(R.id.themeBtn)
+        switch?.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if(isChecked){
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+            }
+            else{
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+            }
+        }
     }
 
     override fun onCreateView(
@@ -21,6 +37,5 @@ class SettingsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
-
    
 }
